bbe9a383014540554fb1996ace32bce3649f6e21
Md. Majharul Islam <majharul.islam03@northsouth.edu>
2024-04-27 15:55:15 +0600

    api testing with postman

--- server/src/server.js
+++ server/src/server.js
@@ -4,12 +4,22 @@ const app = express();
 
 app.use(morgan("dev"))
 
-
 app.get('/test', (req, res)=>{
     res.status(200).send({
         "message": "welcome to server! "
     })
 })
+// app.post('/test',(req,res)=>{
+//     res.status(200).send({
+//         message: "post: api testing"
+//     })
+// })
+
+// app.put('/test',(req,res)=>{
+//     res.status(200).send({
+//         message: "put: api testing"
+//     })
+// })
 
 
 app.listen(3001, ()=>{
