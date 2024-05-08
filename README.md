e7aff7a83000d02a5df6bac039b3638377c4ce86
Md. Majharul Islam <majharul.islam03@northsouth.edu>
2024-05-08 15:24:28 +0600

    express error handling middleware

--- README.md
+++ README.md
@@ -17,6 +17,8 @@ npm install express
 3. Create express server
 4. HTTP request & response
 5. nodemon and morgan package
+6. Middleware
+7. Express Error Handling Middleware
 
 
 
--- server/src/server.js
+++ server/src/server.js
@@ -1,14 +1,48 @@
 const express = require('express')
 const morgan = require('morgan')
+const bodyParser = require('body-parser')
 const app = express();
 
 app.use(morgan("dev"))
+
+//express build-in middleware
+// https://expressjs.com/en/guide/using-middleware.html#middleware.built-in
+// app.use(express.json()); //req body json niye kaj korar jonno express.json() use korlam
+// app.use(express.urlencoded({ extended: true}))//req body te form type data niye kaj korte express.urlencoded use korlam
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}))
+
+const isLoggedIn = (req,res,next)=>{
+
+    const login = true;
+    if(login){
+        req.body.id = 101;
+        next();
+    }else{
+        return res.status(401).json({
+            message: "Please login first"
+        })
+    }
+    
+}
+
+
 
 app.get('/test', (req, res)=>{
     res.status(200).send({
         "message": "welcome to server! "
     })
 })
+
+app.get('/api/userProfile', isLoggedIn, (req,res)=>{
+    console.log(req.body.id)
+    res.status(200).send({
+        message: 'user profile is required',
+        userId: req.body.id,
+    })
+})
+
 // app.post('/test',(req,res)=>{
 //     res.status(200).send({
 //         message: "post: api testing"
@@ -20,6 +54,27 @@ app.get('/test', (req, res)=>{
 //         message: "put: api testing"
 //     })
 // })
+
+
+//Client Error handling
+app.use((req,res,next)=>{
+    res.status(404).send({
+        message: 'Routes not found.'
+
+    })
+
+})
+
+//Server Error handling
+app.use((err,req,res,next)=>{
+    console.error(err.stack);
+    res.status(404).json(
+        'Something broke'
+    )
+})
+
+
+
 
 
 app.listen(3001, ()=>{
